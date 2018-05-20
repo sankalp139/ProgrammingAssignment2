87f19c800ac524284468a40fd579dd4e9e8a99df
sankalp139 <sankalp139@gmail.com>
2018-05-20 10:40:02 +0530

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,33 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) { i<-NULL
-  set<-function(y)
+makeCacheMatrix <- function(x = matrix()) { i<-NULL ##cache matrix function is used for setting the value of inverse
+  set<-function(y)##set funbction is used to set value
   {
     x<<-y
-    i<<-NULL
+    i<<-NULL             
   }
-get<-function()x
-setinverse<-function(inverse)
+get<-function()x ## get function is used to get the value directly
+setinverse<-function(inverse) ## it helps in setting the inverse
 {
   i<<-inverse
   
 }
 getinverse<- function()
   i
-list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)## inverse values and original values are set
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {   ##finds out inverse matrix using makeCacheMatrix function
         ## Return a matrix that is the inverse of 'x'
   
   i<-x$getinverse()
   if(!is.null(i))
   {
-    message("cached data recieved")
+    message("cached data recieved") ## data is accepted from the function above
     return(i)
   }
   data<-x$get()
